491d8ee9143d63946eb1e088bed684ae2ec9cb7a
Jeremiah Via <jeremiah.via@gmail.com>
2013-02-13 11:08:54 -0500

    Re-organize repo.

--- project.clj
+++ project.clj
@@ -2,4 +2,8 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [incanter "1.4.1"]])
+                 [incanter "1.4.1"]]
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  :resource-paths ["src/main/resources"]
+  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"])
