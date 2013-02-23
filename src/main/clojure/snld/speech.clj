23658adc46c5160de4813904266eaef25b9b878e
Jeremiah Via <jeremiah.via@gmail.com>
2013-02-23 14:21:37 -0500

    Add output from frontend as an example.

--- src/main/clojure/snld/speech.clj
+++ src/main/clojure/snld/speech.clj
@@ -1,11 +1,3 @@
-;; Need:
-;; - DONE signal processor
-;; - TODO speech decoder
-;; - TODO speech model: P(W)
-;; - TODO language model: P(A|W))
-
-;; http://stackoverflow.com/questions/7674877/how-to-get-frequency-from-fft-result
-
 (ns ^{:doc "Convert acoustic signals to text."
       :author "Jeremiah Via"}
   snld.speech
@@ -76,17 +68,20 @@
           :else (recur (data frontend)
                        (conj accm (prep-data val))))))
 
-(defn file [url]
-  (File. url))
-
 (defn audio-file-feature-extractor
   "Given a path to an audio file, extract all features from it for use
-  in mode building."
+  in model building."
   [path]
   (let [frontend (make-pipline)
         audio (audio-file-data-source :file path)]
     (extract-features frontend audio)))
 
+(def ef
+  {:timestamp 1361569455108, :sample-rate 16000,
+   :features
+   {:cepstrum '(-0.6350142 0.93213284 0.0996516 -0.5628229 -0.009311179 0.47833294 -0.18784887 -0.2874116 -0.3183234 -0.3713474 -0.10051748 0.1914295 0.12308788),
+    :delta '(5.394108 -0.192472 -0.7899849 0.1037459 -1.9239167E-4 -0.038117655 -0.506319 -0.286837 0.12566972 0.16874354 0.0024353901 0.49930543 -0.07753961),
+    :ddelta '(-0.4581273 -0.49824384 0.17722619 0.09057022 0.16333143 -0.50345284 -0.065142944 0.4884249 0.26812062 0.20054768 0.13845621 -0.1357249 -0.28731254)}})
 
 (defn where-am-i? []  (.getCanonicalPath (File. ".")))
 
@@ -94,13 +89,6 @@
 (defn aw-prob
   "The probability of the acoustic sequence given the "
   [A W])
-
-
-;; Papers on HMM for computing a statistical representation of an
-;; acoustic signal
-;; - Baum, 1972
-;; - Baker, 1975
-;; - Jelinek, 1976
 
 (defn bigram
   "Computes the bigram probabilities on given corpora.
